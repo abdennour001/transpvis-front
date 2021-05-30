8d4a766505a4a03ebd631b4fcbf2ca431a41e206
abdennour001 <amokranabdennour@gmail.com>
2021-05-30 10:51:31 +0100

    add env variable

--- src/utils/environment.js
+++ src/utils/environment.js
@@ -1,6 +1,17 @@
-export const environment = {
-    production: false,
-    env: "dev",
-    apiEndpoint: "http://127.0.0.1:8000/api/"
-    // wsEndpoint: "ws://127.0.0.1:8000/ws/",
-};
+let environment = {};
+
+if (process.env.REACT_APP_ENV === "dev") {
+    environment = {
+        production: false,
+        env: "dev",
+        apiEndpoint: "http://127.0.0.1:8000/api/"
+    };
+} else {
+    environment = {
+        production: true,
+        env: "prod",
+        apiEndpoint: "https://transpvis-back.herokuapp.com/api/"
+    };
+}
+
+export { environment };
